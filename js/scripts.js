f73b76d2a8bc5a4bee3c99df126975cb83d59aab
Celeste Grobey <celestegrobey@gmail.com>
2022-10-21 12:41:01 -0700

    wip trying to remove hidden attribute

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,21 @@
 function handleRadio(event) {
   event.preventDefault();
-  const radioSelection =document.querySelector("input[name='bev']:checked").value
+  const radioSelection =document.querySelector("input[name='bev']:checked").value;
 
-}
+  const go = document.getElementById("go");
+  go.setAttribute("class","hidden");
+  const python = document.getElementById("python");
+  python.setAttribute("class","hidden");
+  const ruby = document.getElementById("ruby");
+  ruby.setAttribute("class","hidden");
+  const javascript = document.getElementById("javascript");
+  javascript.setAttribute("class","hidden");
 
-window.addEventListener("load", function(){
-  this.document.getElementById("questions").addEventListener("submit", handleRadio)
-})
+  window.addEventListener("load", function(){
+  document.getElementById("questions").addEventListener("submit", handleRadio)})
 
-let go = document.getElementById("go");
-go.setAttribute("class","hidden");
-const python = document.getElementById("python");
-python.setAttribute("class","hidden");
-const ruby = document.getElementById("ruby");
-ruby.setAttribute("class","hidden");
-const javascript = document.getElementById("javascript");
-javascript.setAttribute("class","hidden");
+    if (radioSelection===document.getElementById("input#milk"))
+      {javascript.removeAttribute("class", "hidden")};
+  }
+
+
