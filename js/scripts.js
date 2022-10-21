705191b9764f78962a677a2b0b95356551974735
Celeste Grobey <celestegrobey@gmail.com>
2022-10-21 13:49:38 -0700

    wip trying to unhide results

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
-function handleRadio(event) {
-  event.preventDefault();
-  const radioSelection =document.querySelector("input[name='bev']:checked").value;
-
+function radioSelection()
   const go = document.getElementById("go");
   go.setAttribute("class","hidden");
   const python = document.getElementById("python");
@@ -11,11 +8,15 @@ function handleRadio(event) {
   const javascript = document.getElementById("javascript");
   javascript.setAttribute("class","hidden");
 
-  window.addEventListener("load", function(){
-  document.getElementById("questions").addEventListener("submit", handleRadio)})
 
-    if (radioSelection===document.getElementById("input#milk"))
-      {javascript.removeAttribute("class", "hidden")};
-  }
+function unhideResults() 
+  const radioSelection =document.querySelector("input[name='bev']:checked").value;
+  if (radioSelection === "input#milk")
+    javascript.removeAttribute("class");
 
-
+    
+  
+window.addEventListener("load"), function(event){
+window.document.getElementById("questions").addEventListener("submit", event)
+  event.preventDefault;
+}
