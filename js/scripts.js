afea81b81bfe42d9edf7ea46910fc98124773d81
Celeste Grobey <celestegrobey@gmail.com>
2022-10-21 10:48:03 -0700

    wip trying to create submit event listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
-function hideResults(event) {
+function handleSelect(event) {
+  document.addEventListener("submit" function)
   event.preventDefault();
+  const milk = document.getElementById("input[name='milk']:checked").value;
+  const tea = document.getElementById("input[name='tea']:checked").value;
+  const water = document.getElementById("input[name='water']:checked").value;
+  const coffee = document.getElementById("input[name='coffee']:checked").value;
+
+  if (milk=checked){
+    javascript.removeAttribute("class")
+  }
   
-
 }
 
 
-}
+  
