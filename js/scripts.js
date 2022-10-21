79dcda1bb895eab7e879a5ba2e9ff36f7a4621ae
Celeste Grobey <celestegrobey@gmail.com>
2022-10-21 16:50:02 -0700

    wip creating functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,26 @@
-function radioSelection(){
+window.addEventListener("load", function(){
+  let form= document.querySelector("form");
   const radioSelection =document.querySelector("input[name='bev']:checked").value;
-}
-function unHideResults(){
-  const go = document.getElementById("go");
-  go.setAttribute("class","hidden");
-  const python = document.getElementById("python");
-  python.setAttribute("class","hidden");
-  const ruby = document.getElementById("ruby");
-  ruby.setAttribute("class","hidden");
-  const javascript = document.getElementById("javascript");
-  javascript.setAttribute("class","hidden");
 
-  const milk = document.getElementById("milk");
-    if (document.getElementById('milk').checked); {
-      javascript.removeAttribute("class", "hidden"); }
-
-form.addEventListener("submit")
-let event=("submit")
-  event.preventDefault
-}
+  form.addEventListener("submit", function(event);{
+    const go = document.getElementById("go");
+    go.setAttribute("class","hidden");
+    const python = document.getElementById("python");
+    python.setAttribute("class","hidden");
+    const ruby = document.getElementById("ruby");
+    ruby.setAttribute("class","hidden");
+    const javascript = document.getElementById("javascript");
+    javascript.setAttribute("class","hidden");
+    const milk = document.getElementById("milk");
+    if (document.getElementById("milk").checked); {
+      javascript.removeAttribute("class", "hidden"); 
+    }
+    if (document.getElementById("tea").checked); {
+      go.removeAttribute("class", "hidden");
+    }
+    if (document.getElementById("water").checked); {
+      python.removeAttribute("class", "hidden");
+    }
+    if (document.getElementById("coffee").checked); {
+      ruby.removeAttribute("class", "hidden");}
+    }
