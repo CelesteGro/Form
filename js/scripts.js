fdba0485eed69e4c8cba3df5fa14bc4853ced93a
Celeste Grobey <celestegrobey@gmail.com>
2022-10-21 12:06:23 -0700

    set attributes to hidden

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,11 @@ window.addEventListener("load", function(){
   this.document.getElementById("questions").addEventListener("submit", handleRadio)
 })
 
+let go = document.getElementById("go");
+go.setAttribute("class","hidden");
+const python = document.getElementById("python");
+python.setAttribute("class","hidden");
+const ruby = document.getElementById("ruby");
+ruby.setAttribute("class","hidden");
+const javascript = document.getElementById("javascript");
+javascript.setAttribute("class","hidden");
